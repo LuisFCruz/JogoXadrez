71de41903b8e3e639086c80378183741b34c0cd3
Luis Fernando da Cruz <luis.cruz@s2it.com.br>
2018-10-10 00:07:04 -0300

    [refactor] adição do dados da posição no dataset

--- src/js/tabuleiro/tabuleiro.controller.js
+++ src/js/tabuleiro/tabuleiro.controller.js
@@ -7,7 +7,7 @@ export class TabuleiroController {
       for (let j = 0; j < 8; j++) {
         const n = i % 2 ? 0 : 1;
         const className = `casa-${(j - n) % 2 ? 'branca' : 'preta'}`;
-        const casa = document.createElement("div")
+        const casa = document.createElement('div');
         casa.id = `c${i}${j}`;
         casa.classList.add(className);
         tabuleiro.appendChild(casa);
@@ -16,6 +16,9 @@ export class TabuleiroController {
   }
 
   posicionarPecas(x, y, className) {
-    document.querySelector(`#c${x}${y}`).classList.add(className);
+    const casa = document.querySelector(`#c${x}${y}`);
+    casa.classList.add(className);
+    casa.dataset.posicaoX = x;
+    casa.dataset.posicaoY = y;
   }
 }
